cd1879620094b113ce94e4b9844b986fe69da7ef
NOISE <46085246+rcy1314@users.noreply.github.com>
2021-02-08 00:39:26 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -35,7 +35,7 @@ class Bot
     // chat_id sign key
     public string $key = "abc";
     //Bot Token
-    public string $token = "1679016407:AAEHII057c26C5_vY8tSLbO8G6mnyXUOYbM";
+    public string $token = "1454810052:AAEiLHY7q0hbo_EMVAfojdZqTROlbBqmjkI";
 
     public function sendMessage($data): array
     {
